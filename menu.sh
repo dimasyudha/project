bc389b05d1ec1fab1301838b6796217f924c1a5d
dimasyudha <yudhadimas06@gmail.com>
2023-09-27 10:50:31 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# download script
+cd /usr/sbin
+wget -O clearcache "https://raw.githubusercontent.com/Rerechan02/v/main/menu/clearcache.sh"
+wget -O delete "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/tendang.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/Rerechan02/v/main/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/Rerechan02/v/main/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/Rerechan02/v/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/Rerechan02/v/main/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/Rerechan02/v/main/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/Rerechan02/v/main/menu/bw.sh"
+wget -O xp "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/Rerechan02/v/main/acs-set.sh"
+wget -O sshws "https://raw.githubusercontent.com/Rerechan02/v/main/ssh/sshws.sh"
+wget -O status "https://raw.githubusercontent.com/Rerechan02/v/main/status.sh"
+wget -O jam "https://raw.githubusercontent.com/Rerechan02/v/main/jam.sh"
+wget -O dmenu "https://raw.githubusercontent.com/Rerechan02/a/main/menu/dmenu.sh"
+wget -O menu "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu.sh"
+wget -O nsmenu "https://raw.githubusercontent.com/Rerechan02/a/main/menu/nsmenu.sh"
+wget -O pubkey "https://raw.githubusercontent.com/Rerechan02/a/main/menu/pubkey.sh"
+wget -O backup "https://raw.githubusercontent.com/Rerechan02/a/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/Rerechan02/a/main/backup/restore.sh"
+wget -O bmenu "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-bckp.sh"
+wget -O xmenu "https://raw.githubusercontent.com/Rerechan02/a/main/menu/xmenu.sh"
+wget -O tmenu "https://raw.githubusercontent.com/Rerechan02/a/main/menu/tmenu.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-vless.sh"
+wget -O running "https://raw.githubusercontent.com/Rerechan02/a/main/menu/running.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-trgo.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-trojan.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-ssh.sh"
+wget -O usernew "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/delete.sh"
+wget -O tendang "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/tendang.sh"
+wget -O menu-set "https://raw.githubusercontent.com/Rerechan02/a/main/menu/menu-set.sh"
+wget -O add-host "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/add-host.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/Rerechan02/a/main/xray/certv2ray.sh"
+wget -O speedtest "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/speedtest_cli.py"
+wget -O restart "https://raw.githubusercontent.com/Rerechan02/a/main/menu/restart.sh"
+wget -O xp "https://raw.githubusercontent.com/Rerechan02/a/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/Rerechan02/a/main/acs-set.sh"
+wget -O status "https://raw.githubusercontent.com/Rerechan02/a/main/status.sh"
+wget -O add-ws "https://raw.githubusercontent.com/Rerechan02/a/main/xray/add-ws.sh"
+wget -O trialvmess "https://raw.githubusercontent.com/Rerechan02/a/main/xray/trialvmess.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/Rerechan02/a/main/xray/renew-ws.sh"
+wget -O del-ws "https://raw.githubusercontent.com/Rerechan02/a/main/xray/del-ws.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/Rerechan02/a/main/xray/cek-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/Rerechan02/a/main/xray/add-vless.sh"
+wget -O trialvless "https://raw.githubusercontent.com/Rerechan02/a/main/xray/trialvless.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/Rerechan02/a/main/xray/renew-vless.sh"
+wget -O del-vless "https://raw.githubusercontent.com/Rerechan02/a/main/xray/del-vless.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/Rerechan02/a/main/xray/cek-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/Rerechan02/a/main/xray/add-tr.sh"
+wget -O trialtrojan "https://raw.githubusercontent.com/Rerechan02/a/main/xray/trialtrojan.sh"
+wget -O del-tr "https://raw.githubusercontent.com/Rerechan02/a/main/xray/del-tr.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/Rerechan02/a/main/xray/renew-tr.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/Rerechan02/a/main/xray/cek-tr.sh"
+wget -O addtrgo "https://raw.githubusercontent.com/Rerechan02/a/main/xray/addtrgo.sh"
+wget -O trialtrojango "https://raw.githubusercontent.com/Rerechan02/a/main/xray/trialtrojango.sh"
+wget -O deltrgo "https://raw.githubusercontent.com/Rerechan02/a/main/xray/deltrgo.sh"
+wget -O renewtrgo "https://raw.githubusercontent.com/Rerechan02/a/main/xray/renewtrgo.sh"
+wget -O cektrgo "https://raw.githubusercontent.com/Rerechan02/a/main/xray/cektrgo.sh"
+chmod +x *
+chmod +x /usr/sbin/*
+cd
+
+
+cat > /etc/cron.d/re_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 2 * * * root /sbin/reboot
+END
+
+cat > /etc/cron.d/xp_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/bin/xp
+END
+
+cat > /home/re_otm <<-END
+7
+END
